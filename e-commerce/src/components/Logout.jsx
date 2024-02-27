3d3691f9f59599794d4cd991350a7b885665cb44
Sjpandya0210 <Sjpandya1991@gmail.com>
2024-02-26 21:31:20 -0500

    drafted Register component

---
+++ e-commerce/src/components/Logout.jsx
@@ -0,0 +1,8 @@
+function Logout () {
+    return (
+        <div>
+            <h2>Logout Page</h2>
+        </div>
+    )
+}
+export default Logout;
---
+++ e-commerce/src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+function Navbar () {
+    return (
+        <div>
+            <h2>Navbar</h2>
+        </div>
+    )
+}
+export default Navbar;
